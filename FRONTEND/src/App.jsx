9869a568fb8f03d09b19c48d43d5392581db8e31
lakshay <lakshaybahrti5160@gmail.com>
2026-03-13 16:39:04 +0530

    Connect frontend to deployed Render backend

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/NAVBAR";
-import Footer from "./components/FOOTER";
-import Home from "./pages/HOME";
-import About from "./pages/ABOUT";
-import SubmitComplaint from "./pages/SUBMIT_COMPLAINT";
-import AdminDashboard from "./pages/ADMIN_DASHBOARD";
-import ComplaintDetails from "./pages/COMPLAINT_DETAILS";
+import Navbar from "./components/NAVBAR.jsx";
+import Footer from "./components/FOOTER.jsx";
+import Home from "./pages/HOME.jsx";
+import About from "./pages/ABOUT.jsx";
+import SubmitComplaint from "./pages/SUBMIT_COMPLAINT.jsx";
+import AdminDashboard from "./pages/ADMIN_DASHBOARD.jsx";
+import ComplaintDetails from "./pages/COMPLAINT_DETAILS.jsx";
 
 function App() {
   return (
